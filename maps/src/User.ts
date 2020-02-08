e6734f959feda8a1af1422e405cd820c195da585
mehditaibi <mr.mehdi.taibi@gmail.com>
2020-02-07 19:38:56 -0800

    added design patterns

--- maps/src/User.ts
+++ maps/src/User.ts
@@ -1,3 +1,24 @@
-class User { 
-  
+import faker from "faker";
+import { Mappable } from "./CustomMap";
+
+export class User implements Mappable {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+  color: string;
+
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude())
+    };
+    this.color = "blue";
+  }
+
+  markerContent(): string {
+    return `User Name: ${this.name}`;
+  }
 }
