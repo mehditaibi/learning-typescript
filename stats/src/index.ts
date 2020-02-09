7cf93c2ed5704c3eae2bdb8868524ca7a8cd60ab
mehditaibi <mr.mehdi.taibi@gmail.com>
2020-02-08 16:26:56 -0800

    reformatted csv file data

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,20 +1,18 @@
-import fs from "fs";
+import { CsvFileReader } from "./CsvFileReader";
+import { MatchResult } from "./MatchResult";
 
-const matches = fs
-  .readFileSync("football.csv", {
-    encoding: "utf-8"
-  })
-  .split("\n")
-  .map((row: string): string[] => {
-    return row.split(",");
-  });
+const reader = new CsvFileReader("football.csv");
+reader.read();
+
+const dateOfFirstMatch = reader.data[0];
+console.log(dateOfFirstMatch);
 
 let manUnitesWins = 0;
 
-for (let match of matches) {
-  if (match[1] === "Man United" && match[5] === "H") {
+for (let match of reader.data) {
+  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
     manUnitesWins++;
-  } else if (match[2] === "Man United" && match[5] == "A") {
+  } else if (match[2] === "Man United" && match[5] == MatchResult.AwayWin) {
     manUnitesWins++;
   }
 }
