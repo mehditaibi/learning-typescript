85183b9cddbf298129c70a1c8d90877ffff4566d
mehditaibi <mr.mehdi.taibi@gmail.com>
2020-02-12 17:43:05 -0800

    finsihed first version of typescript + express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,13 @@
 import express, { Request, Response } from "express";
+import { router } from "./routes/loginRoutes";
+import bodyParser from "body-parser";
+import cookieSession from "cookie-session";
 
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send(`
-  <div>
-    <h1>Hi there!</div>
-  </div>
-  `);
-});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ["abcdef"] }));
+app.use(router);
 
 app.listen(3000, () => {
   console.log(`listening on port 3000`);
