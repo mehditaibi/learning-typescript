88fd77bbd8f194b2cf1b2a9f87443c73801bdc91
mehditaibi <mr.mehdi.taibi@gmail.com>
2020-02-13 18:45:05 -0800

    finished advanced server project

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,15 @@
-import express, { Request, Response } from "express";
-import { router } from "./routes/loginRoutes";
+import express  from "express";
 import bodyParser from "body-parser";
 import cookieSession from "cookie-session";
+import { AppRouter } from "./AppRouter";
+import "./controllers/LoginController";
+import "./controllers/RootControler";
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ["abcdef"] }));
-app.use(router);
+app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
   console.log(`listening on port 3000`);
